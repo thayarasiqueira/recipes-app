4b48afcdc47b5a400c29a71064e3dd285ec30dff
Heitor da Costa <heitor2000costa@gmail.com>
2022-06-20 18:12:15 -0300

    requisitos 67, 68, 69 concluidos

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Explore from './pages/Explore';
+import ExploreDrinks from './pages/ExploreDrinks';
+import ExploreFoods from './pages/ExploreFoods';
 
 function App() {
   return (
     <Switch>
-      <Route exact path="/" component={ Login } />
+      {/* <Route exact path="/" component={ Login } />
       <Route path="/foods" component={ Foods } />
       <Route path="/drinks" component={ Drinks } />
-      <Route path="/explore" component={ Explore } />
-      <Route path="/profile" component={ Profile } />
+      <Route path="/profile" component={ Profile } /> */}
+      <Route exact path="/explore" component={ Explore } />
+      <Route path="/explore/foods" component={ ExploreFoods } />
+      <Route path="/explore/drinks" component={ ExploreDrinks } />
     </Switch>
   );
 }
