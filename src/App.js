85ff83e77d4600c5f06a9f2d7287262947eb93db
Thayara Siqueira <thay.siqu@gmail.com>
2022-06-22 11:39:25 -0300

    req 9: cria header e organiza arquivos em diretorios

--- src/App.js
+++ src/App.js
@@ -3,21 +3,23 @@ import { Switch, Route } from 'react-router-dom';
 import Login from './Telas/Login';
 import TelaPrincipal from './Telas/TelaPrincipal';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Explore from './pages/Explore';
-import ExploreDrinks from './pages/ExploreDrinks';
-import ExploreFoods from './pages/ExploreFoods';
-import Profile from './pages/Profile';
+import Explore from './Telas/Explore';
+import ExploreDrinks from './Telas/ExploreDrinks';
+import ExploreFoods from './Telas/ExploreFoods';
+import Profile from './Telas/Profile';
+import InProgress from './Telas/InProgress';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
       <Route path="/foods" component={ TelaPrincipal } />
-      {/* <Route path="/drinks" component={ Drinks } />
-      <Route path="/profile" component={ Profile } /> */}
+      <Route path="/drinks" component={ TelaPrincipal } />
+      {/* <Route path="/drinks" component={ Drinks } /> */}
       <Route exact path="/explore" component={ Explore } />
       <Route path="/explore/foods" component={ ExploreFoods } />
       <Route path="/explore/drinks" component={ ExploreDrinks } />
+      <Route path="/foods/:id/in-progress" component={ InProgress } />
       <Route path="/profile" component={ Profile } />
     </Switch>
   );
