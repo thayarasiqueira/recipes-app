390dbaff2fdf478ecafabc2d0aeead26992295e4
Isaac Franzen <isaac_lol_@outlook.com>
2022-06-22 16:04:57 -0300

    tela de login feita

--- src/Telas/Login.js
+++ src/Telas/Login.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import LoginContext from '../context/LoginContext';
 
 function Login() {
+  const {
+    userEmail,
+    handlechangeEmail,
+    saveEmailStorage,
+    handlechangePassword,
+    disabled } = useContext(LoginContext);
   return (
-    <body>
-      Teste
-    </body>
+    <div>
+      <form>
+        <input
+          type="email"
+          placeholder="email"
+          data-testid="email-input"
+          value={ userEmail.email }
+          onChange={ handlechangeEmail }
+        />
+        <input
+          type="password"
+          placeholder="senha"
+          data-testid="password-input"
+          onChange={ handlechangePassword }
+        />
+        <Link to="/foods">
+          <button
+            type="button"
+            disabled={ disabled }
+            data-testid="login-submit-btn"
+            onClick={ saveEmailStorage }
+          >
+            Login
+
+          </button>
+        </Link>
+      </form>
+    </div>
 
   );
 }
